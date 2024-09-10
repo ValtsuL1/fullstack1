91c89ad9a1f26db60b448e42cafa4ea32abff8bb
ValtsuL1 <linnavaltteri@gmail.com>
2024-09-10 12:51:40 +0300

    started adding frontpage

---
+++ src/Title.tsx
@@ -0,0 +1,18 @@
+
+
+function Title() {
+  return (
+    <>
+      <h1 style={{
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        width: '100%'
+      }}>
+        TEST
+      </h1>
+    </>
+  )
+}
+
+export default Title
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Title from './title.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Title/>
     <App />
   </StrictMode>,
 )
