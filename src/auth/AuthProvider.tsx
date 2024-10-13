f545079b4d0a1a7ed68e8bae311f19886f608dc4
ValtsuL1 <linnavaltteri@gmail.com>
2024-10-13 13:58:23 +0300

    added AuthProvider

---
+++ src/auth/AuthProvider.tsx
@@ -0,0 +1,68 @@
+import { createContext, useContext, useState } from "react";
+import { json, useNavigate } from "react-router-dom";
+
+type LoginType = {
+    username: string
+    email: string
+    password: string
+    remember: boolean | undefined
+}
+
+interface ProviderProps {
+    user: string | null
+    token: string,
+    login (data: LoginType): void,
+    logout(): void,
+}
+
+const AuthContext = createContext<ProviderProps>({
+    user: null,
+    token: '',
+    login: () => {},
+    logout: () => {}
+})
+
+export const randomToken = (length: number) => {
+    let s = ''
+    Array.from({ length }).some(() => {
+        s += Math.random().toString(36).slice(2)
+        return s.length >= length
+    })
+    return s.slice(0, length)
+}
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const storedInfo = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '{}') : null
+    const [user, setUser] = useState<string | null>(storedInfo?.email || '')
+    const [token, setToken] = useState(storedInfo?.token || '')
+    const navigate = useNavigate()
+
+    const login = (data: LoginType) => {
+        const t = randomToken(60)
+        setTimeout(() => {
+            const obj = {...data, token: t}
+            setUser(data.email)
+            setToken(t)
+            localStorage.setItem('user', JSON.stringify(obj))
+            navigate('/')
+        }, 1000)
+    }
+
+    const logout = () => {
+        setUser(null)
+        setToken('')
+        localStorage.removeItem('user')
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, token, login, logout }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
+
+export const useAuth = () => {
+    return useContext(AuthContext)
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,70 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 function Login() {
+
+    interface FormElements extends HTMLFormControlsCollection {
+        username: HTMLInputElement
+        email: HTMLInputElement
+        password: HTMLInputElement
+    }
+
+    interface FormElement extends HTMLFormElement {
+        readonly elements: FormElements
+    }
+
+    const [input, setInput] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+
+    const handleSubmit = (e: React.FormEvent<FormElement>) => {
+        e.preventDefault();
+        if (input.username !== "" && input.password !== "" && input.email !== "") {
+            console.log(input.username)
+        }
+        alert("input not valid")
+    }
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+        setInput((prev) => ({
+            ...prev,
+            [name]: value,
+        }))
+    }
+    
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text"/>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        onChange={handleInput}
+                    />
+                </label>
+                <label>
+                    <p>Email</p>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        onChange={handleInput}
+                    />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password"/>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        onChange={handleInput}
+                    />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
