4e56d19c6f8403138810112d3562f4902e9f3317
ValtsuL1 <linnavaltteri@gmail.com>
2024-10-22 12:25:50 +0300

    added header, username to header

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,7 +35,6 @@ function Login() {
                 }).then(res => res.json()).then(res => {
                     sessionStorage.setItem('token', res.access_token)
                     localStorage.setItem('user_id', res.user_id)
-                    localStorage.setItem('username', res.username)
                 })
                 navigate("/")
                 return true 
